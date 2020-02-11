f50d00c136b387b104a2411d2031b9f89abcbaad
hanguyen <hanguyen271.93@gmail.com>
2020-02-11 14:48:01 +0700

    Updated database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Task.destroy_all
+20.times do |index|
+  Task.create(description: Faker::Lorem.sentence(20, false, 0).chop, pickup_loc: Faker::Address.full_address, deliver_loc: Faker::Address.full_address, status: 'new')
+end
